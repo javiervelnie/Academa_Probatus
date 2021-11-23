ec300b58625fdaed5c0df22f063b00e3cde2780f
javivn99 <javiyosoyyo@gmail.com>
2021-11-23 20:09:28 +0100

    no funciona el pdo

---
+++ dist/php/pdoconfig.php
@@ -0,0 +1,5 @@
+<?php
+    $host = 'https://rhll.appian.community/database/sql.php?db=Appian&table=aptmistareas&pos=0';
+    $dbname = 'Appian';
+    $username = 'javiiervnieto99@gmail.com';
+    $password = 'nagasaki99';
