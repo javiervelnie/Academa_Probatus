321ce04430cf6df2e2a886453c8f82bf5190cca1
javivn99 <javiyosoyyo@gmail.com>
2021-11-23 19:56:21 +0100

    añadidos los logos en el login y en el registro y añadidfa alguna funcioonlidad al boton cancelarf

---
+++ dist/js/registro.js
@@ -0,0 +1,7 @@
+//FUNCION PARA QUE EL BOTON DEL LOGIN TE REGRESE AL LOGIN
+const cancelButton = document.querySelector("#btn_cancelar");
+cancelButton.addEventListener("click", redirigirToLogin);
+
+function redirigirToLogin() {
+    window.location.href = "login.html";
+}
