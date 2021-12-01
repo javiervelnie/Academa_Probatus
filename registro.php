62e6da943ee40b8f751e26989b1bc1d097397e2d
javivn99 <javiyosoyyo@gmail.com>
2021-12-01 21:18:03 +0100

    ya saca los mensajes de error correspondientes

--- registro.php
+++ registro.php
@@ -33,76 +33,46 @@ require_once "dist/php/databaseconect.php";
 
 
 
+    if (isset($_POST['btn_crear'])) {
 
-if (isset($_POST['btn_crear'])) {
+        $consulta = $conn->prepare("INSERT INTO alumno(dni,nombre,apellidos,email,password1,password2) VALUES (:dni,:nombre,:apellidos,:email,:password1,:password2)");
 
-    $consulta = $conn->prepare("INSERT INTO alumno(dni,nombre,apellidos,email,password1,password2) VALUES (:dni,:nombre,:apellidos,:email,:password1,:password2)");
+        /* RECOGER VARIABLES DEL FORMULARIO */
+        $nombre = $_POST['nombre'];
+        $apellidos = $_POST['apellidos'];
+        $email = $_POST['email'];
+        $dni = $_POST['dni'];
+        $password1 = $_POST['password1'];
+        $password2 = $_POST['password2'];
 
-    /* RECOGER VARIABLES DEL FORMULARIO */
-    $nombre = $_POST['nombre'];
-    $apellidos = $_POST['apellidos'];
-    $email = $_POST['email'];
-    $dni = $_POST['dni'];
-    $password1 = $_POST['password1'];
-    $password2 = $_POST['password2'];
-
-    /* EXPRESIONES REGULARES */
-    $nombre_check = preg_match("/^[A-Z]{1}[a-z]+|[A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+$/", $nombre);
-    $apellidos_check = preg_match("/^[A-Z]{1}[a-z]+|[A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+$/", $apellidos);
-    $email_check = preg_match("/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[_a-z0-9-]+(.[_a-z0-9-]+)*(.[a-z]{2,4})$/", $email);
-    $dni_check = preg_match("/^\d{8}[a-zA-Z]$/", $dni);
-    $password_check = preg_match('/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/', $password1); 
-    //entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. 
-    //Puede tener otros símbolos. 
-    $password_equal = $password1 ==  $password2;
+        /* EXPRESIONES REGULARES */
+        $nombre_check = preg_match("/^[A-Z]{1}[a-z]+|[A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+$/", $nombre);
+        $apellidos_check = preg_match("/^[A-Z]{1}[a-z]+|[A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+$/", $apellidos);
+        $email_check = preg_match("/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[_a-z0-9-]+(.[_a-z0-9-]+)*(.[a-z]{2,4})$/", $email);
+        $dni_check = preg_match("/^\d{8}[a-zA-Z]$/", $dni);
+        $password_check = preg_match('/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/', $password1);
+        //entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. 
+        //Puede tener otros símbolos. 
+        $password_equal = $password1 ==  $password2;
 
 
-    $consulta->bindParam(':dni', $dni);
-    $consulta->bindParam(':nombre', $nombre);
-    $consulta->bindParam(':apellidos', $apellidos);
-    $consulta->bindParam(':email', $email);
-    $consulta->bindParam(':password1', $password1);
-    $consulta->bindParam(':password2', $password2);
+        $consulta->bindParam(':dni', $dni);
+        $consulta->bindParam(':nombre', $nombre);
+        $consulta->bindParam(':apellidos', $apellidos);
+        $consulta->bindParam(':email', $email);
+        $consulta->bindParam(':password1', $password1);
+        $consulta->bindParam(':password2', $password2);
 
-    if ($nombre_check && $apellidos_check && $email_check && $dni_check && $password_check && $password_equal) {
-        if ($consulta->execute()) {
-            echo "Datos guardados correctamente";
+        if ($nombre_check && $apellidos_check && $email_check && $dni_check && $password_check && $password_equal) {
+            if ($consulta->execute()) {
+                echo'<script type="text/javascript">
+                        window.location.href="login.php";
+                        alert("Usuario creado con exito");
+                    </script>';
+            } else {
+                echo "Error al añadir.";
+            }
         } else {
-            echo "Error al añadir.";
-        }
-    } else{
-        if(!$nombre_check){echo 'error en el nombre';}
-        if(!$apellidos_check){echo 'error en los apellidos';}
-        if(!$email_check){echo 'error en el email';}
-        if(!$dni_check){echo 'error en el dni';}
-        if(!$password_check){echo 'error formato contraseña';}
-        if(!$password_equal){echo 'error contraseñas no son iguales';}
-    }
-}
-
-/*
-if (!empty($_POST['crear'])) {
-    $dni = $_POST['dni'];
-    $nombre = $_POST['nombre'];
-    $apellidos = $_POST['apellidos'];
-    $email = $_POST['email'];
-    $password1 = $_POST['password1'];
-    $password2 = $_POST['password2'];*/
-/* Regular expression check */
-/*$email_check = preg_match('~^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+.([a-zA-Z]{2,4})$~i', $email);
-    $password_check = preg_match('~^[A-Za-z0-9!@#$%^&*()_]{6,20}$~i', $password1);
-    $password_check = preg_match('~^[A-Za-z0-9!@#$%^&*()_]{6,20}$~i', $password2);*/
-
-//if ($email_check && $password_check) {
-/* $id = $userClass->userRegistration($dni, $nombre, $apellidos, $email, $password1, $password2);
-        if ($id) {
-            $url = BASE_URL . 'login.php';
-            header("Location: $url"); // Page redirecting to login.php 
-        } else {
-            $errorMsgReg = "Username or Email already exists.";
-        }*/
-//}
-//}
 
 ?>
 
@@ -126,32 +96,63 @@ if (!empty($_POST['crear'])) {
         <div id="titulo">
             <h2>Registro</h2>
         </div>
-        <form id="datos" action="" method="POST">
+        <form id="datos" action="" method="POST" name="formulario">
 
             <div class="columna">
                 <div class="datos derecha">
                     <label>Nombre</label><input type="text" name="nombre" required="true">
+                    <?php
+                        if (!$nombre_check) {
+                            echo '<div class="errores">Error en el nombre</div>';
+                        }
+                    ?>
                 </div>
                 <div class="datos izquierda">
                     <label>Apellidos</label><input type="text" name="apellidos" required="true">
+                    <?php
+                        if (!$apellidos_check) {
+                            echo '<div class="errores">Error en los apellidos</div>';
+                        }
+                    ?>
                 </div>
             </div>
 
             <div class="columna">
                 <div class="datos derecha">
                     <label>Email</label><input type="text" name="email" required="true">
+                    <?php
+                        if (!$email_check) {
+                            echo '<div class="errores">Error en el email</div>';
+                        }
+                    ?>
                 </div>
                 <div class="datos izquierda">
                     <label>NIF</label><input type="text" name="dni" required="true">
+                    <?php
+                        if (!$dni_check) {
+                            echo '<div class="errores">Error en el dni</div>';
+                        }
+                    ?>
                 </div>
             </div>
 
             <div class="columna">
                 <div class="datos derecha">
                     <label>Contraseña</label><input type="password" name="password1" required="true">
+                    <?php
+                        if (!$password_check) {
+
+                            echo '<div class="errores"><p>8-16 caracteres. Min una min y mayus.</p> <p>Puede contener simbolos.</p></div>';
+                        }
+                    ?>
                 </div>
                 <div class="datos izquierda">
                     <label>Repita su contraseña</label><input type="password" name="password2" required="true">
+                    <?php
+                        if (!$password_equal) {
+                            echo '<div class="errores">La contraseña no coincide.</div>';
+                        }
+                    ?>
                 </div>
             </div>
 
@@ -168,3 +169,8 @@ if (!empty($_POST['crear'])) {
 </body>
 
 </html>
+
+<?php
+}
+}
+?>
